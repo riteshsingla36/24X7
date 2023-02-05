57a2358731d1b0ff13f4e940e23eacca2d428962
riteshsingla36 <ritesh.singla36@gmail.com>
2023-02-05 23:27:30 +0530

    updated carousel

--- src/components/HomepageCarousel.jsx
+++ src/components/HomepageCarousel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const HomepageCarousel = () => {
   return (
@@ -7,38 +8,47 @@ const HomepageCarousel = () => {
       <Carousel.Item>
         <img
           crossOrigin="anonymous"
+          style={{ height: "300px", objectFit: "cover" }}
           className="d-block w-100"
           src="/images/carousel/carousel-1.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <LinkContainer style={{cursor: 'pointer'}} to='product-details'>
+            <h3>First slide label</h3>
+          </LinkContainer>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           crossOrigin="anonymous"
+          style={{ height: "300px", objectFit: "cover" }}
           className="d-block w-100"
           src="/images/carousel/carousel-1.png"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <LinkContainer style={{cursor: 'pointer'}} to='product-details'>
+            <h3>Second slide label</h3>
+          </LinkContainer>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           crossOrigin="anonymous"
+          style={{ height: "300px", objectFit: "cover" }}
           className="d-block w-100"
           src="/images/carousel/carousel-1.png"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <LinkContainer style={{cursor: 'pointer'}} to='product-details'>
+            <h3>Third slide label</h3>
+          </LinkContainer>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
